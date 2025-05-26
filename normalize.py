515de2416cb5a19a31edf778eb0e2ab563d89616
zmj2 <111396281+zmj2@users.noreply.github.com>
2025-05-26 23:35:08 +0800

    Update normalize.py

--- normalize.py
+++ normalize.py
@@ -1,12 +1,18 @@
 import re
 import pkuseg
+from candidate_generator.component_map import load_component_dict
 
 LETTER_BLOCK_RE = re.compile(r'[a-zA-Z]+(?:\s[a-zA-Z]+)*')  # 拼音+空格块
 PUNCT_RE = re.compile(r'[。，！？；：“”《》、,.!?]')
 HANZI_BLOCK_RE = re.compile(r'[\u4e00-\u9fff]+')
 
-# 初始化 pkuseg 分词器
 seg = pkuseg.pkuseg()
+
+try:
+    component_dict = load_component_dict("candidate_generator/dict/component_map.txt")
+    component_keys = set(component_dict.keys())
+except:
+    component_keys = set()
 
 def tokenize(text):
     tokens = []
@@ -23,6 +29,18 @@ def tokenize(text):
         if m:
             tokens.append(m.group().strip())
             i = m.end()
+            continue
+        
+        # 尝试匹配偏旁组合（最长优先匹配，最大长度设为3）
+        matched = False
+        for l in [3, 2]:
+            sub = text[i:i + l]
+            if sub in component_keys:
+                tokens.append(sub)
+                i += l
+                matched = True
+                break
+        if matched:
             continue
 
         # 中文块（交给 pkuseg）
@@ -48,5 +66,5 @@ def tokenize(text):
 
 
 if __name__ == "__main__":
-    text = "doubi 脸太 gwi则了！zgr太强"
+    text = "doubi 脸太 gwi则了！zgr太强。禾斗匕匕。"
     print("分词结果：", tokenize(text))
